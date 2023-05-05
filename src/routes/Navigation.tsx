e104a80af14d5de425e1f88095fb7c895815d00c
farpires <arpirez.fabio@gmail.com>
2023-05-05 13:12:00 -0300

    Component compound ready

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -4,8 +4,8 @@ import {
   Route,
   NavLink
 } from 'react-router-dom';
-
 import logo from '../logo.svg';
+import { ShoppingPage } from '../02-component-patterns/pages/ShoppingPage';
 
 export const Navigation = () => {
   return (
@@ -15,7 +15,7 @@ export const Navigation = () => {
             <img src={ logo } alt="React Logo" />
           <ul>
             <li>
-              <NavLink to="/" activeClassName="nav-active" exact>Home</NavLink>
+              <NavLink to="/" activeClassName="nav-active" exact>shopping</NavLink>
             </li>
             <li>
               <NavLink to="/about" activeClassName="nav-active" exact>About</NavLink>
@@ -36,7 +36,7 @@ export const Navigation = () => {
             <h1>Users</h1>
           </Route>
           <Route path="/">
-            <h1>Home</h1>
+            <ShoppingPage />
           </Route>
         </Switch>
       </div>
